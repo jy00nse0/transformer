275284d14630d2e90a37668016103d53d079e807
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-29 03:18:58 +0900

    Update decoder.py

--- models/model/decoder.py
+++ models/model/decoder.py
@@ -7,18 +7,17 @@ import torch
 from torch import nn
 
 from models.blocks.decoder_layer import DecoderLayer
-from models.embedding.transformer_embedding import TransformerEmbedding
+from models.embedding.embedding import Embedding
 
 
 class Decoder(nn.Module):
     def __init__(self, dec_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
         super().__init__()
-        self.emb = TransformerEmbedding(d_model=d_model,
+        self.emb = Embedding(d_model=d_model,
                                         drop_prob=drop_prob,
                                         max_len=max_len,
                                         vocab_size=dec_voc_size,
                                         device=device)
-
         self.layers = nn.ModuleList([DecoderLayer(d_model=d_model,
                                                   ffn_hidden=ffn_hidden,
                                                   n_head=n_head,
