e76200911839d986c516f8f687d41239186cd859
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-29 03:20:39 +0900

    Update positional_encoding.py

--- models/embedding/positional_encoding.py
+++ models/embedding/positional_encoding.py
@@ -5,13 +5,13 @@
 """
 import torch
 from torch import nn
-
+from models.embedding.positional_encoding import PositionalEncoding
 
 class PositionalEncoding(nn.Module):
     """
     compute sinusoid encoding.
     """
-
+    
     def __init__(self, d_model, max_len, device):
         """
         constructor of sinusoid encoding class
@@ -29,15 +29,12 @@ class PositionalEncoding(nn.Module):
         pos = torch.arange(0, max_len, device=device)
         pos = pos.float().unsqueeze(dim=1)
         # 1D => 2D unsqueeze to represent word's position
-
+        # # 0부터 d_model 직전까지: $0, 1, 2, ..., d_{model}-1$ 범위 내에서step=2: 2씩 건너뛰며 값을 생성
         _2i = torch.arange(0, d_model, step=2, device=device).float()
-        # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
-        # "step=2" means 'i' multiplied with two (same with 2 * i)
 
         self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
         self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
-        # compute positional encoding to consider positional information of words
-
+    
     def forward(self, x):
         # self.encoding
         # [max_len = 512, d_model = 512]
