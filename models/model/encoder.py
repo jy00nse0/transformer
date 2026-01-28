a94d845c79b119cca587ceb11a22345fe8a54103
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-29 03:18:40 +0900

    Update encoder.py

--- models/model/encoder.py
+++ models/model/encoder.py
@@ -6,19 +6,18 @@
 from torch import nn
 
 from models.blocks.encoder_layer import EncoderLayer
-from models.embedding.transformer_embedding import TransformerEmbedding
+from models.embedding import Embedding
 
 
 class Encoder(nn.Module):
 
     def __init__(self, enc_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
         super().__init__()
-        self.emb = TransformerEmbedding(d_model=d_model,
+        self.emb = Embedding(d_model=d_model,
                                         max_len=max_len,
                                         vocab_size=enc_voc_size,
                                         drop_prob=drop_prob,
                                         device=device)
-
         self.layers = nn.ModuleList([EncoderLayer(d_model=d_model,
                                                   ffn_hidden=ffn_hidden,
                                                   n_head=n_head,
