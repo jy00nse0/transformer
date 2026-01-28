1646ff05da8b6259a3f6429fa018ec2064acf9e9
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-29 03:21:06 +0900

    Update and rename transformer_embedding.py to Embedding.py

--- models/embedding/Embedding.py
+++ models/embedding/Embedding.py
@@ -3,13 +3,17 @@
 @when : 2019-10-22
 @homepage : https://github.com/gusdnd852
 """
+
+"""
+@author : Hyunwoong
+@when : 2019-10-22
+@homepage : https://github.com/gusdnd852
+"""
 from torch import nn
-
 from models.embedding.positional_encoding import PositionalEncoding
-from models.embedding.token_embeddings import TokenEmbedding
 
 
-class TransformerEmbedding(nn.Module):
+class Embedding(nn.Module):
     """
     token embedding + positional encoding (sinusoid)
     positional encoding can give positional information to network
@@ -22,12 +26,15 @@ class TransformerEmbedding(nn.Module):
         :param vocab_size: size of vocabulary
         :param d_model: dimensions of model
         """
-        super(TransformerEmbedding, self).__init__()
-        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        super(Embedding, self).__init__()
+        #TokenEmbedding
+        self.tok_emb = nn.Embedding(vocab_size, d_model, padding_idx=1)
         self.pos_emb = PositionalEncoding(d_model, max_len, device)
         self.drop_out = nn.Dropout(p=drop_prob)
 
     def forward(self, x):
         tok_emb = self.tok_emb(x)
         pos_emb = self.pos_emb(x)
+        #  we apply dropout to the sums of the embeddings and the positional encodings in both the encoder and decoder stacks.
         return self.drop_out(tok_emb + pos_emb)
+
